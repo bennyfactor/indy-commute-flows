8d1edafdb1e97415f314b041eb2d2285b14d0cdc
Ben Lamb <ben@t420.local>
2026-06-24 11:56:57 -0400

    Deploy interactive map (LODES 2023) to GitHub Pages

--- scripts/00-verify-env.R
+++
@@ -1,9 +0,0 @@
-# scripts/00-verify-env.R
-pkgs <- c("mapgl","lehdr","tigris","sf","dplyr","htmlwidgets")
-for (p in pkgs) {
-  v <- as.character(packageVersion(p))
-  cat(sprintf("%-12s %s\n", p, v))
-}
-stopifnot("mapgl >= 0.5.0 required" = packageVersion("mapgl") >= "0.5.0")
-stopifnot("add_flowmap missing" = "add_flowmap" %in% getNamespaceExports("mapgl"))
-cat("ENV OK\n")
